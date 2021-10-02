7af18ce32338a253c78822359c8accdbea5ebf9e
Daniel Weidensdoerfer <daniel.weidensdoerfer@gmail.com>
2021-10-02 12:58:07 +0200

    Initial Commit

---
+++ app/src/main/java/com/titaniel/materialyoucolorpaletteviewer/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.titaniel.materialyoucolorpaletteviewer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.titaniel.materialyoucolorpaletteviewer.ui.theme.MaterialYouColorPaletteViewerTheme
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialYouColorPaletteViewerTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
